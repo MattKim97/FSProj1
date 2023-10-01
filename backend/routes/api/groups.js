ad916139192298d75b6cb6ce8ccda2e4d0344a4c
mattk <mkimucsb@gmail.com>
2023-09-30 18:40:24 -0700

    set-up routes

---
+++ backend/routes/api/groups.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const { Op } = require('sequelize');
+const bcrypt = require('bcryptjs');
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
+
+const { Group } = require('../../db/models');
+
+const router = express.Router();
+
+
+module.exports = router;
